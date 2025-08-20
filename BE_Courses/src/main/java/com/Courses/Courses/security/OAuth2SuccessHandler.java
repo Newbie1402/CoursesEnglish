254b916b6f6331b970ec1d4641d1c54941f01047
Ho4FireDragon <anhoa1794@gmail.com>
2025-08-20 11:48:15 +0700

    [FIX] fix config in OAuth2SuccessHandler

--- BE_Courses/src/main/java/com/Courses/Courses/security/OAuth2SuccessHandler.java
+++ BE_Courses/src/main/java/com/Courses/Courses/security/OAuth2SuccessHandler.java
@@ -22,6 +22,7 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -32,7 +33,7 @@ import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
-public class OAuth2SuccessHandler implements org.springframework.security.web.authentication.AuthenticationSuccessHandler {
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     @Autowired
     private final UsersRepository usersRepository;
@@ -52,8 +53,6 @@ public class OAuth2SuccessHandler implements org.springframework.security.web.au
     @Autowired
     private AcceptedAccountRepository acceptedAccountRepository;
 
-    @Value("${app.frontend.url:${APP_FRONTEND_URL}}")
-    private String frontendUrl;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -115,16 +114,14 @@ public class OAuth2SuccessHandler implements org.springframework.security.web.au
 
         String jwt = jwtService.generateToken(userDetails);
         String encodedToken = URLEncoder.encode(jwt, StandardCharsets.UTF_8);
-<<<<<<< HEAD
+
 
         String redirectUri = "http://localhost:5173/oauth2/redirect"
                 + "?token=" + encodedToken
                 + "&userId=" + user.getId()
                 + "&studentId=" + (studentId != null ? studentId : "null")
                 + "&teacherId=" + (teacherId != null ? teacherId : "null");
-=======
-        String redirectUri = frontendUrl + "/oauth2/redirect?token=" + encodedToken + "&userId=" + user.getId();
->>>>>>> f5ddd327c57b4f068e9fb60363368cb5735b0f12
+
 
         response.sendRedirect(redirectUri);
     }
